fccd9ca0fd286684536b34bcc8f88d55acd4dc25
Denilson <denilsonmvs@gmail.com>
2025-01-28 22:30:57 -0300

    Retirando recursão

--- main.py
+++ main.py
@@ -5,85 +5,145 @@ import sys
 def load_file(filename: str):
     lines = open(filename, "r").readlines()
 
-    num_variables = None    
     num_clauses = None
 
     current_line = 0
     while True:
-        parts = lines[current_line].split(" ")
+        parts = " ".join(lines[current_line].split()).split(" ")
         current_line += 1
 
         if parts[0] == "p":
-            num_variables = int(parts[2])
             num_clauses = int(parts[3])
             break
 
     clauses: list[set[int]] = []
-    for _ in range(num_clauses):
-        clauses.append([int(value) for value in lines[current_line].split(" ")[:-1]])
-        current_line += 1
-
-    return clauses, num_variables
-
-
-
-def set_variable(clauses: list[list[int]], num_variables, variable: int):
-
-    new_clauses = []
-    for clause_idx in range(len(clauses)):
-        clause = clauses[clause_idx]
-        if variable in clause:
+    while num_clauses > 0:
+        if lines[current_line][0] == "c":
+            current_line += 1
             continue
 
-        new_clause = [var for var in clause if var != -variable]
-        if len(new_clause) == 0:
-            return None
+        clauses.append([int(value) for value in " ".join(lines[current_line].split()).split(" ")[:-1]])
+        current_line += 1
+        num_clauses -= 1
 
-        new_clauses.append(new_clause)
-    
-    return recursion_step(new_clauses, num_variables)
+    return clauses
 
-def recursion_step(clauses: list[list[int]], num_variables: int):
-    if len(clauses) == 0:
-        return []
-    
-    pure_verification = [[False, False] for _ in range(num_variables + 1)]
+
+def set_propagating_value(
+    clauses: list[list[int]],
+    variable_values: list[int],
+    decision_stack: list[tuple[int, bool]],
+    value_to_set: int
+):
+    variable_values[abs(value_to_set)] = value_to_set
+    decision_stack.append((value_to_set, False))
 
     for clause in clauses:
-        if len(clause) == 1:
-            return set_variable(clauses, num_variables, clause[0])
-        for variable in clause:
-            if variable < 0:
-                pure_verification[-variable][0] = True
-            else:
-                pure_verification[variable][1] = True
-    
-    for var in range(len(pure_verification)):
-        has_neg, has_pos = pure_verification[var]
-        if has_neg and not has_pos:
-            return set_variable(clauses, num_variables, -var)
-        elif not has_neg and has_pos:
-            return set_variable(clauses, num_variables, var)
-    
-    result = set_variable(clauses, num_variables, clauses[0][0])
-    if result is None:
-        result = set_variable(clauses, num_variables, -clauses[0][0])
+        solved = False
+        free_variables = []
         
-        if result is None:
-            return None
-        else:
-            result.append(-clauses[0][0])
-            return result
-    
-    else:
+        for var in clause:
+            value = variable_values[abs(var)]
+            if var == value:
+                solved = True
+                break
 
-        result.append(clauses[0][0])
-        return result
+            if value == 0:
+                free_variables.append(var)
+        
+        if not solved and len(free_variables) == 0:
+            variable_values[abs(value_to_set)] = 0
+            decision_stack.pop()
+            return False
+
+    return True
+
+
+def propagate(
+    clauses: list[list[int]],
+    variable_values: list[int],
+    decision_stack: list[tuple[int, bool]]
+):
+    while True:
+        value_to_set = None
+
+        for clause in clauses:
+
+            solved = False
+            free_variables = []
+            for var in clause:
+                value = variable_values[abs(var)]
+                if var == value:
+                    solved = True
+                    break
+
+                if variable_values[abs(var)] == 0:
+                    free_variables.append(var)
+
+            if not solved:
+                if len(free_variables) == 1:
+                    value_to_set = free_variables[0]
+                    break
+                elif len(free_variables) == 0:
+                    return False
+
+        if value_to_set is None:
+            break
+
+        # adiciono a clausula olhando as clausulas que estão finalizadas que possuem o literal
+        if not set_propagating_value(clauses, variable_values, decision_stack, value_to_set):
+            return False
+            
+    return True
+
+def decide(clauses: list[list[int]], variable_values: list[int], decision_stack: list[tuple[int, bool]]):
+    for clause in clauses:
+        for var in clause:
+            value = variable_values[abs(var)]
+            if value == var:
+                break
+
+            if value == 0:
+                variable_values[abs(var)] = var
+                decision_stack.append((var, True))
+                return True
+    return False
+
+def get_biggest_variable(clauses: list[list[int]]):
+    res = 0
+    for clause in clauses:
+        for var in clause:
+            res = max(res, abs(var))
+    return res
+
+def solve(clauses: list[list[int]]):
+    num_variables = get_biggest_variable(clauses)
+    variable_values = [0 for _ in range(num_variables + 1)]
+
+    decision_stack = []
+
+    while True:
+        if propagate(clauses, variable_values, decision_stack):
+            if not decide(clauses, variable_values, decision_stack):
+                return True
+        else:
+            while len(decision_stack) > 0:
+                (value, from_decide) = decision_stack.pop()
+                variable_values[abs(value)] = 0
+
+                if from_decide:
+                    decision_stack.append((-value, False))
+                    variable_values[abs(value)] = -value
+                    break
+
+            if len(decision_stack) == 0:
+                return False        
+
 
 
 def main():
-    clauses, num_variables = load_file(sys.argv[1])
-    print(recursion_step(clauses, num_variables))
+    clauses = load_file(sys.argv[1])    
+    print(solve(clauses))
 
 
 
---
+++ run_sats.sh
@@ -0,0 +1,24 @@
+python3 main.py pj1-tests/sat/block0.cnf
+python3 main.py pj1-tests/sat/prime169.cnf
+python3 main.py pj1-tests/sat/sqrt1042441.cnf
+python3 main.py pj1-tests/sat/uf20-0103.cnf
+# python3 main.py pj1-tests/sat/prime1849.cnf
+python3 main.py pj1-tests/sat/sqrt10609.cnf
+python3 main.py pj1-tests/sat/uf20-0104.cnf
+python3 main.py pj1-tests/sat/cnfgen-php-10-10.cnf
+# python3 main.py pj1-tests/sat/prime841.cnf
+python3 main.py pj1-tests/sat/sqrt11449.cnf
+python3 main.py pj1-tests/sat/uf20-0105.cnf
+python3 main.py pj1-tests/sat/elimredundant.cnf
+# python3 main.py pj1-tests/sat/prime961.cnf
+python3 main.py pj1-tests/sat/uf20-01000.cnf
+python3 main.py pj1-tests/sat/uf20-0106.cnf
+python3 main.py pj1-tests/sat/prime121.cnf
+python3 main.py pj1-tests/sat/sat10.cnf
+python3 main.py pj1-tests/sat/uf20-0100.cnf
+# python3 main.py pj1-tests/sat/prime1369.cnf
+python3 main.py pj1-tests/sat/sat12.cnf
+python3 main.py pj1-tests/sat/uf20-0101.cnf
+# python3 main.py pj1-tests/sat/prime1681.cnf
+python3 main.py pj1-tests/sat/sqrt10201.cnf
+python3 main.py pj1-tests/sat/uf20-0102.cnf
